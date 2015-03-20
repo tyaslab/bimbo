1b1f6df9587e5868ca76892dab8b968eecfd2e99
tyaslab <tyaslab.com@gmail.com>
2015-03-20 08:55:01 +0700

    first commit

---
+++ settings.py
@@ -0,0 +1,8 @@
+DEBUG=True
+SECRET_KEY='th15-15-4-53c123t-k3y'
+ROOT_URLCONF='urls'
+MIDDLEWARE_CLASSES=(
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+)
