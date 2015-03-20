a7536cc0e8fea43b23ece04915e966b02f0126b5
tyaslab <tyaslab.com@gmail.com>
2015-03-20 12:47:23 +0700

    media

--- settings.py
+++ settings.py
@@ -72,7 +72,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'app/static'),
 )
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'app/media')
 MEDIA_URL = '/media/'
 
 TEMPLATE_DIRS = (
--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, patterns
+from django.conf import settings
+from django.conf.urls.static import static
 from django.http import HttpResponse
 
 urlpatterns = patterns('',
     url(r'^$', lambda x: HttpResponse('Hello World')),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
